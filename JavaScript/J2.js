ee7bb7236c282e8e8874fefa3919435a898ce3df
jeremhuy28 <jeremhuy28@g.crookcountyschools.org>
2026-01-15 14:33:13 -0800

    done

--- JavaScript/J2.js
+++ JavaScript/J2.js
@@ -1,347 +1,96 @@
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
-
-
-
 const tableElement = document.getElementById('sales-table');
-
-
-
-
-
-
-
-// TODO:  create a comment below describing how this Constructor function works
-// The contructor funtion creates a sum of all the numbers from a location using 'locationName", 'mincustomsperhour', maxcustomersperhour', and ' avgcookiespersale' as peremeters. giving the sum of 'totaldailycookies'
-
-// - what makes it a constructor function?
-// - This makes it a constructor function because it uses the input from other objects to create a new object, aka the sum of the input from that said location.
-
-// - what do you think the CookieStand.all.push(all) method is doing?
-// It pushes all of the 'this' objects above it into 'cookiestand.all' array.
-
 function CookieStand(locationName, minCustomersPerHour, maxCustomersPerHour, avgCookiesPerSale) {
-
-
  this.locationName = locationName;
-
-
  this.minCustomersPerHour = minCustomersPerHour;
-
-
  this.maxCustomersPerHour = maxCustomersPerHour;
-
-
  this.avgCookiesPerSale = avgCookiesPerSale;
-
-
  this.customersEachHour = [];
-
-
  this.cookiesEachHour = [];
-
-
  this.totalDailyCookies = 0;
-
-
  CookieStand.all.push(this);
-
-
 }
-
-
-
-
-
-
-
-// TODO: create a comment describing the method below
-// The method is taking the array's (CookieStand) 'customersEachHour' and 'cookiesEachHour' and pushing into the function/object
-
-// - what does prototype mean?
-// Prototype lets you use the an array to male another object using those said arrays without having to create an individual array for that object.
-
 CookieStand.prototype.calcCustomersEachHour = function() {
-
-
  for (let i = 0; i < hours.length; i++) {
-
-
    this.customersEachHour.push(random(this.minCustomersPerHour, this.maxCustomersPerHour));
-
-
  }
-
-
 };
-
-
-
-
-
-
-
 CookieStand.prototype.calcCookiesEachHour = function() {
-
-
  this.calcCustomersEachHour();
-
-
  for (let i = 0; i < hours.length; i++) {
-
-
    const oneHour = Math.ceil(this.customersEachHour[i] * this.avgCookiesPerSale);
-
-
    this.cookiesEachHour.push(oneHour);
-
-
    this.totalDailyCookies += oneHour;
-
-
  }
-
-
 };
-
-
-
-
-
-
-
 CookieStand.prototype.render = function() {
-
-
  this.calcCookiesEachHour();
-
-
  const tableRow = document.createElement('tr');
-
-
  let tableDataElement = document.createElement('td');
-
-
  tableDataElement.textContent = this.locationName;
-
-
  tableRow.appendChild(tableDataElement);
-
-
  for (let i = 0; i < hours.length; i++) {
-
-
    tableDataElement = document.createElement('td');
-
-
    tableDataElement.textContent = this.cookiesEachHour[i];
-
-
    tableRow.appendChild(tableDataElement);
-
-
  }
-
-
  const tableHeaderElement = document.createElement('th');
-
-
  tableHeaderElement.textContent = this.totalDailyCookies;
-
-
  tableRow.appendChild(tableHeaderElement);
-
-
  tableElement.appendChild(tableRow);
-
-
 };
-
-
-
-
-
-
 
 CookieStand.all = [];
-
-
-
-
-
-
-
-// TODO: instantiate a new CookieStand object (with sample data) for Dubai, Paris, and Lima
-
-
 new CookieStand('Seattle', 23, 65, 6.3);
-
 new CookieStand('Tokyo', 3, 24, 1.2);
-
 new CookieStand('Dubai', 2, 28, 3.1);
-
 new CookieStand('Paris', 14, 64, 8);
-
 new CookieStand('Lima', 28, 42, 4.8);
-
 function random(min, max) {
-
-
  return Math.floor(Math.random() * (max - min + 1)) + min;
-
-
 }
-
-
-
-
-
-
-
 function makeHeaderRow() {
-
-
  const tableRow = document.createElement('tr');
-
-
  let tableHeaderElement = document.createElement('th');
-
-
  tableHeaderElement.textContent = 'Locations';
-
-
  tableRow.appendChild(tableHeaderElement);
-
-
  for (let i = 0; i < hours.length; i++) {
-
-
    tableHeaderElement = document.createElement('th');
-
-
    tableHeaderElement.textContent = hours[i];
-
-
    tableRow.appendChild(tableHeaderElement);
-
-
  }
-
-
  tableHeaderElement = document.createElement('th');
-
-
  tableHeaderElement.textContent = 'Location Totals';
-
-
  tableRow.appendChild(tableHeaderElement);
-
-
  tableElement.appendChild(tableRow);
-
-
 }
-
-
-
-
-
-
-
-// TODO: create a series of comments in this function where you are confused with what's happening
-
-
-// - what about the code is confusing?
-//The equations inside this code confuses me, the names of these variables amplifies my confusion due to me not knowing the idea behind the code.
-
-// - can you guess what it does?
-
-
 function makeFooterRow() {
-
  const tableRow = document.createElement('tr');
-
-
  let tableHeaderElement = document.createElement('th');
-
-
  tableHeaderElement.textContent = 'Hourly Totals for All Locations';
-
-
  tableRow.appendChild(tableHeaderElement);
-
-
  let totalOfTotals = 0;
-
-
  for (let i = 0; i < hours.length; i++) {
-
-
    let hourlyTotal = 0;
-
-
-   for (const j = 0; j < CookieStand.all.length; j++){
-
-// dont know what equation does
+   for (let j = 0; j < CookieStand.all.length; j++){
      hourlyTotal += CookieStand.all[j].cookiesEachHour[i];
-
-
      totalOfTotals += CookieStand.all[j].cookiesEachHour[i];
-
-
    }
-
-
-   tableHeaderElement = document.createElement('th');
-
-
+   tableHeaderElement = document.createElement('th')
    tableHeaderElement.textContent = hourlyTotal;
-
-
    tableRow.appendChild(tableHeaderElement);
-
-
  }
-
-
  tableHeaderElement = document.createElement('th');
-
-
  tableHeaderElement.textContent = totalOfTotals;
-
-
  tableRow.appendChild(tableHeaderElement);
-
-
  tableElement.appendChild(tableRow);
-
-
 }
-
-
-
-
-
-
-
 (function renderTable() {
-
-
  makeHeaderRow();
-
-
  for(let i = 0; i < CookieStand.all.length; i++){
-
-
    CookieStand.all[i].render();
-
-
  }
-
-
  makeFooterRow();
-
-
 })();
 
 
@@ -349,54 +98,17 @@ function makeFooterRow() {
 
 
 
-
 const ocean = document.getElementById('ocean'),
-
-
  waveWidth = 10,
-
-
  waveCount = Math.floor(window.innerWidth/waveWidth),
-
-
  docFrag = document.createDocumentFragment();
-
-
-
-
-
-// i dont know what this entire loop does
-
 for(let i = 0; i < waveCount; i++){
-
-
  const wave = document.createElement('div');
-
-
  wave.className += ' wave';
-
-
  docFrag.appendChild(wave);
-
-
  wave.style.left = i * waveWidth + 'px';
-
-
  wave.style.webkitAnimationDelay = (i/100) + 's';
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
 ocean.appendChild(docFrag);
 
 
